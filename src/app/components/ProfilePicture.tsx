87f97a966c2788b69ff055d20cd848b81aa1a95b
R.paco <167717853+Ruhanpaco@users.noreply.github.com>
2024-10-26 16:29:39 +0200

    Update ProfilePicture.tsx

--- src/app/components/ProfilePicture.tsx
+++ src/app/components/ProfilePicture.tsx
@@ -1,4 +1,4 @@
-// src/app/components/ProfilePicture.tsx
+
 import Image from 'next/image';
 
 export default function ProfilePicture() {
@@ -6,7 +6,7 @@ export default function ProfilePicture() {
     <div className="flex justify-center">
       <div className="w-48 h-48 md:w-56 md:h-56 rounded-full overflow-hidden shadow-lg">
         <Image
-          src="/assets/img/profile-photo.png"  // Ensure the path is correct
+          src="/assets/img/profile-photo.png"  
           alt="Photo"
           width={224}  // Set appropriate width for the image
           height={224} // Set appropriate height for the image
